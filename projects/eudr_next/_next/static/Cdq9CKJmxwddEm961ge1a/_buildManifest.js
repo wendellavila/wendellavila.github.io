f684c829694ea9401580fdb71221b975d269f73f
wendellavila <wendelljcavila@gmail.com>
2024-07-01 17:30:14 +0000

    ci: Automated deployment with GitHub Actions. Source repo: wendellavila/eudr_next

--- projects/eudr_next/_next/static/Cdq9CKJmxwddEm961ge1a/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-8e7471fea8d45c70.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ projects/eudr_next/_next/static/pLDtCsm4DUfBoIYOPfLmx/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Fdashboard","\u002F[lang]\u002Forders","\u002F[lang]\u002Freport","\u002F[lang]\u002Fsettings"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
